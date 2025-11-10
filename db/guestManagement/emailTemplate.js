e0cb41d2893888f5e4784e9dcea8186a0bad6d93
Vijay6484 <vt6484@gmail.com>
2025-11-10 20:11:00 +0530

    changes

---
+++ db/guestManagement/emailTemplate.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const propertyScoped = require('../plugins/propertyScoped');
+
+const emailTemplateSchema = new mongoose.Schema(
+    {
+        template_name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        content: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true },
+);
+
+emailTemplateSchema.plugin(propertyScoped);
+emailTemplateSchema.index({ template_name: 1, property: 1 }, { unique: true });
+
+const EmailTemplateModel =
+    mongoose.models.EmailTemplate || mongoose.model('EmailTemplate', emailTemplateSchema);
+
+module.exports = EmailTemplateModel;
+module.exports.schema = emailTemplateSchema;
+
